0d684fd88d86986c97b86967d5607dbd985cdbcc
Becerra Arancibia Javier <ja.becerraa@gmail.com>
2024-02-11 12:43:41 -0300

    feat(preonic): remove encoder functionality

--- preonic/keymap.c
+++ preonic/keymap.c
@@ -178,33 +178,6 @@ uint16_t muse_counter = 0;
 uint8_t muse_offset = 70;
 uint16_t muse_tempo = 50;
 
-bool encoder_update_user(uint8_t index, bool clockwise) {
-  if (muse_mode) {
-    if (IS_LAYER_ON(_RAISE)) {
-      if (clockwise) {
-        muse_offset++;
-      } else {
-        muse_offset--;
-      }
-    } else {
-      if (clockwise) {
-        muse_tempo+=1;
-      } else {
-        muse_tempo-=1;
-      }
-    }
-  } else {
-    if (clockwise) {
-      register_code(KC_PGDN);
-      unregister_code(KC_PGDN);
-    } else {
-      register_code(KC_PGUP);
-      unregister_code(KC_PGUP);
-    }
-  }
-    return true;
-}
-
 bool dip_switch_update_user(uint8_t index, bool active) {
     switch (index) {
         case 0:
--- preonic/rules.mk
+++ preonic/rules.mk
@@ -1,1 +1,3 @@
+ENCODER_ENABLE = no # Disable the encoder part from compile
+
 SRC += muse.c
